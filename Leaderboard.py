fb6cd9d624b31ca5306ddaf3b175271ebf8eec47
Ehsan_Herosan <ehsan.moeini.stream@gmail.com>
2025-01-01 07:43:16 -0800

    graphical changes + added wins and losses

--- Leaderboard.py
+++ Leaderboard.py
@@ -2,21 +2,42 @@ import keyboard
 import json
 from printy import printy
 
-def leaderboard():
+def load_accounts():  # loading json file content which has users data
     with open('account.json', 'r') as data:
-        users = json.load(data)
+        return json.load(data)
 
+def leaderboard():
+    users = load_accounts()
     sorted_users = sorted(users, key=lambda x: x["points"], reverse=True)
 
-    printy(" ====================TOP 3 PLAYERS======================== ", "BHw")
-    printy("   {:<23}{:<20}{:<12} ".format("username", "points", "play time"), "BH")
+    printy("="*44+"TOP 3 PLAYERS"+"="*44, "BHw")
+    printy(" {:30}{:<20}{:<15}{:<20}{:<15}".format("Username", "Points", "Wins", "Losses", "Play Time"), "BH")
 
-    printy("    {:<24}{:<22}{:<8} ".format(sorted_users[0]['username'], sorted_users[0]['points'],
-                                         sorted_users[0]['play_time']), "BHy")
-    printy("    {:<24}{:<22}{:<8} ".format(sorted_users[1]['username'], sorted_users[1]['points'],
-                                         sorted_users[1]['play_time']), "BHc")
-    printy("    {:<24}{:<22}{:<8} ".format(sorted_users[2]['username'], sorted_users[2]['points'],
-                                         sorted_users[2]['play_time']), "BHm")
+    try:
+        printy("  {:30}{:<+20}{:<15}{:<20}{:<14}".format(sorted_users[0]['username'], sorted_users[0]['points'],
+                                             sorted_users[0]['wins'], sorted_users[0]['losses'],
+                                                         f"{sorted_users[0]['play_time'][0]}h"
+                                                         f"{sorted_users[0]['play_time'][1]}m"
+                                                         f"{sorted_users[0]['play_time'][2]}s"), "BHy")
+    except:
+        pass
+    try:
+        printy("  {:30}{:<+20}{:<15}{:<20}{:<14}".format(sorted_users[1]['username'], sorted_users[1]['points'],
+                                             sorted_users[1]['wins'], sorted_users[1]['losses'],
+                                                         f"{sorted_users[1]['play_time'][0]}h"
+                                                         f"{sorted_users[1]['play_time'][1]}m"
+                                                         f"{sorted_users[1]['play_time'][2]}s"), "BHy")
+    except:
+        pass
+    try:
+        printy("  {:30}{:<+20}{:<15}{:<20}{:<14}".format(sorted_users[2]['username'], sorted_users[2]['points'],
+                                             sorted_users[2]['wins'], sorted_users[2]['losses'],
+                                                         f"{sorted_users[2]['play_time'][0]}h"
+                                                         f"{sorted_users[2]['play_time'][1]}m"
+                                                         f"{sorted_users[2]['play_time'][2]}s"), "BHy")
+    except:
+        pass
+
     while True:
         if keyboard.is_pressed('q'):
             exit()
